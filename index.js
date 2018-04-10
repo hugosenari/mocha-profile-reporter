cb37748d4fa71135abd69e69bb04b2dfd1b607a4
Hugo Ribeiro <hugo.ribeiro@net.com.br>
2018-04-10 17:40:02 -0300

    inherits from Spec

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const mocha = require('mocha');
 
-function MochaProfileReporter(runner) {
-  mocha.reporters.Spec.call(this, runner);
+const Spec = mocha.reporters.Spec;
 
-  runner.on('test', function(test){
+function MochaProfileReporter(runner) {
+  Spec.call(this, runner);
+
+  runner.on('test', (test) => {
     console.profile(test.fullTitle());
   });
 
-  runner.on('test end', function(test, err){
+  runner.on('test end', (test, err) => {
     console.profileEnd(test.fullTitle());
   });
 }
 
+mocha.utils.inherits(MochaProfileReporter, Spec);
 module.exports = MochaProfileReporter;
